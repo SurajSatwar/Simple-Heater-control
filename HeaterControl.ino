af5db57cd399b4660b5d20702a5733bfad45844e
Suraj Satwar <surajsatwar@gmail.com>
2025-07-22 19:52:00 +0530

    Arduino code

--- HeaterControl.ino
+++ HeaterControl.ino
@@ -1,97 +1,144 @@
-#include "DHT.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
-#define DHTPIN 2         // Data pin connected to DHT22
+// LCD setup
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Pin configuration
+#define DHTPIN 2
 #define DHTTYPE DHT22
-#define HEATER_PIN 3     // Heater simulated by LED
-#define BUZZER_PIN 4     // Overheat buzzer
+#define HEATER_PIN 3
+#define BUZZER_PIN 4
+
+// Thresholds
+#define TARGET_TEMP 40
+#define OVERHEAT_TEMP 50
 
 DHT dht(DHTPIN, DHTTYPE);
 
-enum HeaterState {
-  IDLE,
-  HEATING,
-  STABILIZING,
-  TARGET_REACHED,
-  OVERHEAT
-};
+// Enum-style constants for states
+#define STATE_IDLE 0
+#define STATE_HEATING 1
+#define STATE_STABILIZING 2
+#define STATE_TARGET_REACHED 3
+#define STATE_OVERHEAT 4
 
-HeaterState currentState;
+int currentState = STATE_IDLE;
 
-const float TARGET_TEMP = 40.0;
-const float OVERHEAT_TEMP = 50.0;
-float temp = 0;
-
-void setup() {
-  Serial.begin(9600);
-  dht.begin();
+void initPins() {
   pinMode(HEATER_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(HEATER_PIN, LOW);
   digitalWrite(BUZZER_PIN, LOW);
-  currentState = IDLE;
+}
+
+void initLCD() {
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Heater System");
+}
+
+void printState(int state) {
+  switch (state) {
+    case STATE_IDLE:
+      lcd.print("IDLE       ");
+      break;
+    case STATE_HEATING:
+      lcd.print("HEATING    ");
+      break;
+    case STATE_STABILIZING:
+      lcd.print("STABILIZING");
+      break;
+    case STATE_TARGET_REACHED:
+      lcd.print("TARGET OK  ");
+      break;
+    case STATE_OVERHEAT:
+      lcd.print("OVERHEAT!! ");
+      break;
+  }
+}
+
+void controlLogic(float temp) {
+  // State transition
+  if (temp < TARGET_TEMP - 2) {
+    currentState = STATE_HEATING;
+  }
+  else if (temp >= TARGET_TEMP - 2 && temp < TARGET_TEMP) {
+    currentState = STATE_STABILIZING;
+  }
+  else if (temp >= TARGET_TEMP && temp < OVERHEAT_TEMP) {
+    currentState = STATE_TARGET_REACHED;
+  }
+  else if (temp >= OVERHEAT_TEMP) {
+    currentState = STATE_OVERHEAT;
+  }
+
+  // Actuation + LCD display
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
+  lcd.print(temp);
+  lcd.print("C ");
+
+  switch (currentState) {
+    case STATE_IDLE:
+      digitalWrite(HEATER_PIN, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
+      printState(STATE_IDLE);
+      break;
+
+    case STATE_HEATING:
+      digitalWrite(HEATER_PIN, HIGH);
+      digitalWrite(BUZZER_PIN, LOW);
+      printState(STATE_HEATING);
+      break;
+
+    case STATE_STABILIZING:
+      digitalWrite(HEATER_PIN, HIGH);
+      digitalWrite(BUZZER_PIN, LOW);
+      printState(STATE_STABILIZING);
+      break;
+
+    case STATE_TARGET_REACHED:
+      digitalWrite(HEATER_PIN, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
+      printState(STATE_TARGET_REACHED);
+      break;
+
+    case STATE_OVERHEAT:
+      digitalWrite(HEATER_PIN, LOW);
+      digitalWrite(BUZZER_PIN, HIGH);
+      printState(STATE_OVERHEAT);
+      break;
+  }
+}
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  dht.begin();
+  initPins();
+  initLCD();
+  currentState = STATE_IDLE;
 }
 
 void loop() {
-  delay(2000); // DHT22 reads every 2 sec
-  temp = dht.readTemperature();
+  float temp = dht.readTemperature();
 
   if (isnan(temp)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Sensor Error");
+    lcd.setCursor(0, 1);
+    lcd.print("Sensor Error    ");
+    delay(2000);
     return;
   }
 
-  // State transition logic
-  if (temp < TARGET_TEMP - 2) {
-    currentState = HEATING;
-  } else if (temp >= TARGET_TEMP - 2 && temp < TARGET_TEMP) {
-    currentState = STABILIZING;
-  } else if (temp >= TARGET_TEMP && temp < OVERHEAT_TEMP) {
-    currentState = TARGET_REACHED;
-  } else if (temp >= OVERHEAT_TEMP) {
-    currentState = OVERHEAT;
-  }
-
-  // Actuator and feedback
-  switch (currentState) {
-    case IDLE:
-      digitalWrite(HEATER_PIN, LOW);
-      digitalWrite(BUZZER_PIN, LOW);
-      break;
-
-    case HEATING:
-      digitalWrite(HEATER_PIN, HIGH);
-      digitalWrite(BUZZER_PIN, LOW);
-      break;
-
-    case STABILIZING:
-      digitalWrite(HEATER_PIN, HIGH);
-      digitalWrite(BUZZER_PIN, LOW);
-      break;
-
-    case TARGET_REACHED:
-      digitalWrite(HEATER_PIN, LOW);
-      digitalWrite(BUZZER_PIN, LOW);
-      break;
-
-    case OVERHEAT:
-      digitalWrite(HEATER_PIN, LOW);
-      digitalWrite(BUZZER_PIN, HIGH);
-      break;
-  }
-
-  // Log data
-  Serial.print("Temperature: ");
+  Serial.print("Temp: ");
   Serial.print(temp);
-  Serial.print(" °C\tState: ");
-  printState(currentState);
-}
+  Serial.print(" C, State: ");
+  Serial.println(currentState);
 
-void printState(HeaterState s) {
-  switch (s) {
-    case IDLE: Serial.println("IDLE"); break;
-    case HEATING: Serial.println("HEATING"); break;
-    case STABILIZING: Serial.println("STABILIZING"); break;
-    case TARGET_REACHED: Serial.println("TARGET REACHED"); break;
-    case OVERHEAT: Serial.println("OVERHEAT"); break;
-  }
+  controlLogic(temp);
+  delay(2000);  // Wait 2 seconds between readings
 }
